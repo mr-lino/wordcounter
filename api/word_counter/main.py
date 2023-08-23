b8d76823811b6200c9a2eda51d2593f50c1a84de
Matheus Lino <matheus.lino@alana.ai>
2023-08-23 15:35:24 -0300

    style: run black, isort and flake8

--- api/word_counter/main.py
+++ api/word_counter/main.py
@@ -1,16 +1,16 @@
-from dataclasses import dataclass
 from typing import Annotated
-from fastapi import FastAPI, Body, status
+
+from fastapi import Body, FastAPI, status
 from fastapi.responses import RedirectResponse
 
-
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return RedirectResponse("/docs")
 
+
 @app.post("/word-count", status_code=status.HTTP_200_OK)
 async def word_count(input_text: Annotated[str, Body(embed=True)]):
-
     return {"word_count": len(input_text.split())}
